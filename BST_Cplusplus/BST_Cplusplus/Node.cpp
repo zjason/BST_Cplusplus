2be004723ebd83066cb9247d93ff8c9187d8cfef
zjason <jason.zhao1992@gmail.com>
2016-06-03 00:25:45 -0400

    Finished the implementation of Node class

--- BST_Cplusplus/BST_Cplusplus/Node.cpp
+++ BST_Cplusplus/BST_Cplusplus/Node.cpp
@@ -1,12 +1,59 @@
 #include "Node.h"
 
 
-
-Node::Node()
+template<typename T>
+Node<T>::Node()
 {
+	left = nullptr;
+	right = nullptr;
 }
 
 
-Node::~Node()
+template<typename T>
+Node<T>::Node(T newData, Node<T>* newLeft, Node<T>* newRight)
 {
+	data = newData;
+	left = newLeft;
+	right = newRight;
+}
+
+template<typename T>
+Node<T>::~Node()
+{
+}
+
+template<typename T>
+void Node<T>::setData(T newData)
+{
+	data = newData;
+}
+
+template<typename T>
+void Node<T>::setLeft(Node<T>* newLeft)
+{
+	left = newLeft;
+}
+
+template<typename T>
+void Node<T>::setRight(Node<T>* newRight)
+{
+	right = newRight;
+}
+
+template<typename T>
+T Node<T>::getItem()
+{
+	return data;
+}
+
+template<typename T>
+Node<T>* Node<T>::getLeft()
+{
+	return left;
+}
+
+template<typename T>
+Node<T>* Node<T>::getRight()
+{
+	return right;
 }
--- BST_Cplusplus/BST_Cplusplus/Node.h
+++ BST_Cplusplus/BST_Cplusplus/Node.h
@@ -1,8 +1,32 @@
 #pragma once
+// T is a generic data type
+template <typename T>
 class Node
 {
+// private means only this class (base class) can access or modify the private field
+private:
+	T data;
+	Node *left;
+	Node *right;
+
+// data or method in protected field means only this class and it's 
+// child class (derived class) can access or modify
+protected:
+
+// 
 public:
 	Node();
+	// overloading Node() function for more argument. Overloading means the same 
+	// function name with different arguments
+	Node(T newData, Node<T> *newLeft = nullptr, Node<T> *newRight = nullptr);
 	~Node();
+
+	void setData(T newData);
+	void setLeft(Node<T> *newLeft);
+	void setRight(Node<T> *newRight);
+
+	T getItem();
+	Node<T> *getLeft();
+	Node<T> *getRight();
 };
 
